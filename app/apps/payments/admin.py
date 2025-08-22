ee1939ff526b61527a717840dfe60d37e0dd3e1e
miclemabasie <miclemabasie3@gmail.com>
2025-08-22 05:39:44 +0100

    added admin config for payments

--- app/apps/payments/admin.py
+++ app/apps/payments/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Payment, EscrowReleaseSchedule
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "appointment",
+        "from_user",
+        "to_user",
+        "amount",
+        "currency",
+        "payment_method",
+        "status",
+        "processed_at",
+    )
+    list_filter = ("status", "payment_method", "currency")
+    search_fields = (
+        "appointment__id",
+        "from_user__email",
+        "to_user__email",
+        "external_transaction_id",
+    )
+    readonly_fields = ("processed_at", "released_at")
+
+
+@admin.register(EscrowReleaseSchedule)
+class EscrowReleaseScheduleAdmin(admin.ModelAdmin):
+    list_display = ("appointment", "scheduled_release_time", "status", "processed_at")
+    list_filter = ("status",)
+    search_fields = ("appointment__id",)
