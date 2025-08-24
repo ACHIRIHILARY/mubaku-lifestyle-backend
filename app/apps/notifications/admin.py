c81f5178db11182c175c075d5fbf5bbda2fe5293
miclemabasie <miclemabasie3@gmail.com>
2025-08-24 05:39:44 +0100

    added admin config for notifications

--- app/apps/notifications/admin.py
+++ app/apps/notifications/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Notification, LoyaltyProgram
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "notification_type", "is_read", "created")
+    list_filter = ("notification_type", "is_read", "created")
+    search_fields = ("title", "user__email")
+    ordering = ("-created",)
+
+
+@admin.register(LoyaltyProgram)
+class LoyaltyProgramAdmin(admin.ModelAdmin):
+    list_display = ("client", "provider", "points", "visits", "last_visit")
+    list_filter = ("visits",)
+    search_fields = ("client__user__username", "provider__user__username")
