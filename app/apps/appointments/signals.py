2474ca3aaaec53f8700103e2ff0cbc3dcc38958f
miclemabasie <miclemabasie3@gmail.com>
2025-08-30 05:39:44 +0100

    added signals for translation in the appointments app

---
+++ app/apps/appointments/signals.py
@@ -0,0 +1,12 @@
+from mubaku.services.translation_service import auto_translate_instance
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
+from .models import ProviderAvailabilityException
+
+
+@receiver(post_save, sender=ProviderAvailabilityException)
+def auto_translate_product(sender, instance, created, **kwargs):
+    if created:
+        # Use transaction on_commit to ensure we have an ID
+        transaction.on_commit(lambda: auto_translate_instance(instance, ["reason"]))
