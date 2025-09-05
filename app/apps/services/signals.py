5c51764230a1d472d7dc9dc1501ae5520b86b968
miclemabasie <miclemabasie3@gmail.com>
2025-09-05 05:39:44 +0100

    added signals vices

---
+++ app/apps/services/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
+from mubaku.services.translation_service import auto_translate_instance
+from .models import ServiceCategory, Service
+
+
+@receiver(post_save, sender=ServiceCategory)
+def auto_translate_service_category(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(
+            lambda: auto_translate_instance(instance, ["name", "description"])
+        )
+
+
+@receiver(post_save, sender=Service)
+def auto_translate_service(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(
+            lambda: auto_translate_instance(instance, ["name", "description"])
+        )
