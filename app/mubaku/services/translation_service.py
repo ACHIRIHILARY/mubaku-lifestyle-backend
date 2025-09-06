f598365163bc4c801a8181418608be15dbb9ca68
miclemabasie <miclemabasie3@gmail.com>
2025-09-06 05:39:44 +0100

    updated translation service by removing context

--- app/mubaku/services/translation_service.py
+++ app/mubaku/services/translation_service.py
@@ -2,23 +2,24 @@
 from googletrans import Translator
 from django.conf import settings
 
+translator = Translator()
+
 
 def auto_translate_instance(instance, field_names):
     """
     Automatically translate an instance's fields to all supported languages
     """
-    with Translator() as translator:
-        for lang_code, _ in settings.LANGUAGES:
+    for lang_code, _ in settings.LANGUAGES:
 
-            for field_name in field_names:
+        for field_name in field_names:
 
-                # get the value of the field
-                value = getattr(instance, field_name)
+            # get the value of the field
+            value = getattr(instance, field_name)
 
-                # get the translated value
-                translated_value = translator.translate(value, dest=lang_code).text
+            # get the translated value
+            translated_value = translator.translate(value, dest=lang_code).text
 
-                # set the translated value
-                setattr(instance, f"{field_name}_{lang_code}", translated_value)
+            # set the translated value
+            setattr(instance, f"{field_name}_{lang_code}", translated_value)
 
-        instance.save()
+    instance.save()
