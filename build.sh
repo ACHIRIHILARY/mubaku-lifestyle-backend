de532195152dbbb38bd53c7b72e6f764c92079fb
ACHIRIHILARY <achirihilary44@gmail.com>
2025-10-13 00:31:54 +0100

    fix: Correct build script for Render deployment

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 # exit on error
 set -o errexit
 
-cd app
-
 pip install --upgrade pip
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+python app/manage.py collectstatic --noinput --settings=app.mubaku.settings.prod
+python app/manage.py migrate --settings=app.mubaku.settings.prod
