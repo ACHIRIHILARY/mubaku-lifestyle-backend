4416b5cb4f3cd19cbea5301647c207e163199c16
ACHIRIHILARY <achirihilary44@gmail.com>
2025-10-13 04:41:21 +0100

    adjusted app module

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python app/manage.py collectstatic --noinput --settings=app.mubaku.settings.prod
-python app/manage.py migrate --settings=app.mubaku.settings.prod
+python app/manage.py collectstatic --noinput --settings=mubaku.settings.prod
+python app/manage.py migrate --settings=mubaku.settings.prod
